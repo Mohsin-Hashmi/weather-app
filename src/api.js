03a8a19f15df6bbead51509efff8eb0901b142ca
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-10-24 18:07:41 +0500

    weather-app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6a1bf55d5fmsh62f567b07d1c1f0p18bc91jsnb3d4372bfcc1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY='90d45403236a48a1f394ede0b11d2c80';
